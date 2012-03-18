e9f2afa425ff9b2beb3ae6f588e9b3f1a46aa02c
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-03-19 00:05:00 +0100

    extracted the hashed report in a private method

--- log_analyzer.rb
+++ log_analyzer.rb
@@ -6,18 +6,24 @@ class LogAnalyzer
   def report
     report = [] << "data risultati-2xx risultati-3xx risultati-4xx risultati-5xx"
 
-    results = {}
-    @log.split("/n").each do |each_line|
-      each_line.scan /\[(\d\d\/\w\w\w\/\d\d\d\d).+\].+\"\s(\d\d\d)/ do |date,response_code|
-        results[date] ||= { "2xx" => 0, "3xx" => 0, "4xx" => 0 }
-        results[date]["#{response_code.chars.first}xx"] += 1
-      end
-    end
-
-    results.each do |date,response_codes|
+    build_hash_report_for(@log).each do |date,response_codes|
       report << "#{date}\t#{response_codes['2xx']}\t#{response_codes['3xx']}\t#{response_codes['4xx']}"
     end
 
     report
+  end
+
+  private
+
+  def build_hash_report_for(log)
+    hashed_report = {}
+    log.split("/n").each do |each_line|
+      each_line.scan /\[(\d\d\/\w\w\w\/\d\d\d\d).+\].+\"\s(\d\d\d)/ do |date,response_code|
+        hashed_report[date] ||= { "2xx" => 0, "3xx" => 0, "4xx" => 0 }
+        hashed_report[date]["#{response_code.chars.first}xx"] += 1
+      end
+    end
+    
+    hashed_report
   end
 end
